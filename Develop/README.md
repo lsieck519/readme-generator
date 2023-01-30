7e3bd2add2068797542bd7c399d1266ec99e4dca
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-01-30 16:20:24 -0600

    basic code and first test readme

---
+++ Develop/README.md
@@ -0,0 +1,37 @@
+Test Title
+
+   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+   (https://opensource.org/licenses/MIT)
+
+    ## Description 
+    It's a great project
+
+    ## Table of Contents
+    * [Installation Instructions] (#installation)
+    * [Usage Information] (#usage)
+    * [Contribution Guidelines] (#contributing)
+    * [Testing Instructions] (#tests)
+    * [License] (#license)
+    * [Questions] (#email)
+
+  
+    ## Installation 
+    Install it
+  
+    ## Usage 
+    Use it
+
+    ## Contributing
+    Contribute
+
+    ## Tests
+    npm test
+
+    ## License 
+    MIT
+
+    ## Questions
+    - Email: email@email
+    - GitHub: https://github.com/lsieck519
+
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,106 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// questions for user input
+const promptUser = () => {
+    return inquirer.prompt([
+    {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description explaining the what, why, and how of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter the steps required to install your project:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select a license, if applicable',
+    choices: ['MIT'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines here:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter instructions for testing this application here:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter your Github username:',
+    },
+]);
+};
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+const generateMarkdown = ({title, description, installation, usage, license, contributing, tests, email, username}) =>
+   `${title}
+
+   [![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)]
+   (https://opensource.org/licenses/${license})
+
+    ## Description 
+    ${description}
+
+    ## Table of Contents
+    * [Installation Instructions] (#installation)
+    * [Usage Information] (#usage)
+    * [Contribution Guidelines] (#contributing)
+    * [Testing Instructions] (#tests)
+    * [License] (#license)
+    * [Questions] (#email)
+
+  
+    ## Installation 
+    ${installation}
+  
+    ## Usage 
+    ${usage}
+
+    ## Contributing
+    ${contributing}
+
+    ## Tests
+    ${tests}
+
+    ## License 
+    ${license}
+
+    ## Questions
+    - Email: ${email}
+    - GitHub: https://github.com/${username}
+
+  `;
+  
+
+const init = () => {
+    promptUser()
+      .then((answers) => writeFile('README.md', generateMarkdown(answers)))
+      .then(() => console.log('Successfully created readme'))
+      .catch((err) => console.error(err));
+  };
+
 init();
